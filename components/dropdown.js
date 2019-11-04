99adf2d0c6fff3d70c478e3b6ef68ee29ea025d6
taha <taha@doublemap.com>
2019-11-04 07:57:36 +0100

    add events on dropdownmenu class

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,27 +1,57 @@
-class dropDownMenu() {
+class DropDownMenu {
 
     constructor(containerSelector, listOfValues) {
         this._dropdownContainer = $(containerSelector);
         this._listOfValues = listOfValues;
-        this._menu = null;
         this.updateDisplay();
     }
 
     updateDisplay() {
-        this._menu.remove();
-        
+
+        if(this._menu !== undefined) {
+          this._menu.remove();
+        }
+
+        var listOfValues = this._listOfValues;
         var html = `<div style='background-color:#FFFFFF;
                   overflow-y: scroll;height:20vh;
                   width: containerWidthpx; border:solid 1px;
                   ><span style=''></span>`;
 
-        for (var i = 0; i < list.length; i++) {
-            element = `<span style='width:100%;'>value</span><br>`.replace("value", list[i]);
+        //console.log(this._dropdownContainer);
+        html = html.replace("containerWidthpx", this._dropdownContainer.width()+'px');
+        for (var i = 0; i < listOfValues.length; i++) {
+            let element = `<span style='width:100%;'>value</span><br>`.replace("value", listOfValues[i]);
             html += element;
         }
 
         html += "</div>";
         this._menu = $(html);
+        this._menu.click(function() {
+          console.log("menu click");
+        })
+
+        $('body').append(this._menu);
+
+        var menu = this._menu;
+        this._dropdownContainer.click(function() {
+          menu.toggle();
+        });
+
+        var dropDownObject = this;
+        $( window ).resize(function() {
+          menu.width(dropDownObject._dropdownContainer.width());
+        });
+
+        $( window ).click(function(e) {
+          if (!dropDownObject._dropdownContainer.is(e.target)
+              && dropDownObject._dropdownContainer.has(e.target).length === 0
+              && !menu.is(e.target)
+              && menu.has(e.target).length === 0) {
+            menu.hide();
+          }
+        });
+
     }
 
     updateValues(listOfValues) {
@@ -31,54 +61,5 @@ class dropDownMenu() {
     getValues() {
         return this._listOfValues;
     }
-
-}
-
-
-
-function dropDownMenu(containerSelector, list, callback) {
-
-  var dropdownContainer = $(containerSelector);
-  var html = `<div style='background-color:#FFFFFF;
-              overflow-y: scroll;height:20vh;
-              width: containerWidthpx; border:solid 1px;
-              ><span style=''></span>`;
-
-  html = html.replace('containerWidth', dropdownContainer.width());
-
-  var selectedValue = dropdownContainer.innerText;
-  console.log(selectedValue);
-  var menu;
-  for (var i = 0; i < list.length; i++) {
-    element = `<span style='width:100%;'>value</span><br>`.replace("value", list[i]);
-    html += element;
-  }
-  html += "</div>";
-  menu = $(html);
-  menu.appendTo(document.body);
-  var off = $(containerSelector).offset();
-  menu.offset({top:off.top + dropdownContainer.height(),left:off.left});
-  menu.hide();
-  menu.find('span').each(function() {
-    $(this).click(function(){
-          selectedValue = $(this)[0].innerText;
-          dropdownContainer.html(selectedValue);
-          menu.hide();
-          callback(selectedValue);
-        });
-  });
-
-  dropdownContainer.click(function() {
-    menu.toggle();
-  });
-
-  $(document.body).click(function(e) {
-    if (!dropdownContainer.is(e.target)
-        && dropdownContainer.has(e.target).length === 0
-        && !menu.is(e.target)
-        && menu.has(e.target).length === 0) {
-      menu.hide();
-    }
-  });
 
 }
---
+++ tests/main.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    dropDown = new DropDownMenu(".dropdown", [1,2,3]);
+    dropDown.updateDisplay();
+});
